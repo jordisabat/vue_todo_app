fc7359d7f6bba338893cb46c0196825d392f23aa
Jordi Sabat <jsabat@gmail.com>
2020-11-18 13:22:15 +0100

    feat: add router

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,26 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <router-view />
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos'
 import Header from './components/layout/Header'
-import AddTodo from './components/AddTodo'
-import axios from 'axios'
-
 export default {
   name: 'App',
   components: {
-    Todos,
-    Header,
-    AddTodo
-  },
-  data() {
-    return {
-      todos: []
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      axios
-        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-        .then(() => (this.todos = this.todos.filter((todo) => todo.id != id)))
-        .catch((err) => console.log(err))
-    },
-    addTodo(newTodo) {
-      const { title, completed } = newTodo
-      axios
-        .post('https://jsonplaceholder.typicode.com/todos', {
-          title,
-          completed
-        })
-        .then((res) => (this.todos = [...this.todos, res.data]))
-        .catch((err) => console.log(err))
-    }
-  },
-  created() {
-    console.log('hello')
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos?_limit=10')
-      .then((res) => (this.todos = res.data))
-      .catch((error) => console.log(error))
+    Header
   }
 }
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 </style>
